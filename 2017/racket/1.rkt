8288a9a747ab1c956e17709aac658f26b1b81d54
James Sandridge <jims@radical-ideal.com>
2017-12-03 18:16:49 -0500

    Solution for day 1, task 2

--- 2017/racket/1.rkt
+++ 2017/racket/1.rkt
@@ -54,11 +54,29 @@
 (check-equal? (feedback 2 shift-list-left '(1 2 3 4)) (shift-list-left-n 2 '(1 2 3 4)))
 (check-equal? (feedback 4 shift-list-left '(1 2 3 4)) (shift-list-left-n 4 '(1 2 3 4)))
 
-(define (zip-solution xs)
+(define (zip-solution n xs)
   (let ([sum ((curry apply) +)])
-    (sum (map (λ (h1 h2) (if (equal? h1 h2) h1 0)) xs (shift-list-left xs)))))
+    (sum (map (λ (h1 h2) (if (equal? h1 h2) h1 0)) xs (shift-list-left-n n xs)))))
 
-(check-equal? (original-solution (listify t1)) (zip-solution (listify t1)))
-(check-equal? (original-solution (listify t2)) (zip-solution (listify t2)))
-(check-equal? (original-solution (listify t3)) (zip-solution (listify t3)))
-(check-equal? (original-solution (listify t4)) (zip-solution (listify t4)))
+(check-equal? (original-solution (listify t1)) (zip-solution 1 (listify t1)))
+(check-equal? (original-solution (listify t2)) (zip-solution 1 (listify t2)))
+(check-equal? (original-solution (listify t3)) (zip-solution 1 (listify t3)))
+(check-equal? (original-solution (listify t4)) (zip-solution 1 (listify t4)))
+
+;; The statement for task 2 says all lists will have an even length, so we won't bother checking that
+;; Because we are lazy
+(define (zip-solution-task-2 xs)
+  (let ([shift (/ (length xs) 2)])
+    (zip-solution shift xs)))
+
+(define t5 "1212")
+(define t6 "1221")
+(define t7 "123425")
+(define t8 "123123")
+(define t9 "12131415")
+
+(check-equal? (zip-solution-task-2 (listify t5)) 6)
+(check-equal? (zip-solution-task-2 (listify t6)) 0)
+(check-equal? (zip-solution-task-2 (listify t7)) 4)
+(check-equal? (zip-solution-task-2 (listify t8)) 12)
+(check-equal? (zip-solution-task-2 (listify t9)) 4)
