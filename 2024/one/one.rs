397c865dddaa19f5f945f67e38d85ff931dbd258
Jim Sandridge <jim.sandridge@gmail.com>
2024-12-01 19:45:56 -0500

    day 1 pt 2

--- 2024/one/one.rs
+++ 2024/one/one.rs
@@ -13,8 +13,12 @@ where
 }
 
 fn main() {
+    // for part 1
     let mut evens = BinaryHeap::new();
     let mut odds = BinaryHeap::new();
+
+    // for part 2
+    let mut sorted_odds = Vec::new();
 
     if let Ok(lines) = read_lines("./input.txt") {
         for line in lines.flatten() {
@@ -25,19 +29,34 @@ fn main() {
             if let [even, odd] = numbers[..] {
                 evens.push(Reverse(even));
                 odds.push(Reverse(odd));
+
+                sorted_odds.push(odd);
             }
         }
     }
 
+    sorted_odds.sort();
+
     assert!(evens.len() == odds.len());
 
     let mut s = 0;
+    let mut t = 0;
     while !evens.is_empty() {
         let Reverse(e) = evens.pop().unwrap();
         let Reverse(o) = odds.pop().unwrap();
 
+        // part 1
         s += i64::abs(e - o);
+
+        let low = sorted_odds.partition_point(|x| x < &e);
+        let high = sorted_odds.partition_point(|x| x <= &e);
+
+        let c = (high - low) as i64;
+
+        // part 2
+        t += e * c;
     }
 
     println!("{}", s);
+    println!("{}", t);
 }
